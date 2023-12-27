4865cf529a361a481888c0a4f857e13fe5650498
rahulkandel7 <rahulkandel7@gmail.com>
2023-12-27 12:40:04 +0545

    Initial Commit

---
+++ temperatureConverter/temperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  temperatureConverterApp.swift
+//  temperatureConverter
+//
+//  Created by Rahul Sharma on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct temperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
